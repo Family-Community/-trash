030fba000c778a6208a03e12fcee327256ec7440
Mekdlsdl <ekdlsdl0@hufs.ac.kr>
2023-07-21 15:28:39 +0900

    url 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
+    path('signup/', include('dj_rest_auth.registration.urls')),
 ]
